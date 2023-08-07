c72ca84478e8010e915e485d9840b419e9d79bc7
Joynal Abedin <cfcfahim26@gmail.com>
2023-08-07 20:22:47 +0600

    Model updated and everything working

--- Server/app.js
+++ Server/app.js
@@ -1,21 +1,24 @@
 const axios = require('axios');
 
-// Replace 'http://localhost:5000/predict' with your Flask API endpoint
 const apiUrl = 'http://localhost:5000/predict';
 
 // Input data for prediction
 const data = {
-  area_type: 'Super built-up  Area',
+  area: 'Super built-up Area',
   location: 'Electronic City Phase II',
-  size: '2 BHK',
-  total_sqft: 1056,
+  size: 2,
+  sqft: 1056,
   bath: 2,
+  balcony: 2,
 };
+const queryString = Object.entries(data)
+  .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+  .join('&');
 
 axios
-  .post(apiUrl, data)
+  .post(apiUrl, queryString)
   .then((response) => {
-    console.log('Prediction:', response);
+    console.log('Prediction:', response.data);
   })
   .catch((error) => {
     console.error('Error:', error.message);
